2026211bf2288c41ac31d2c89b89859fa4085090
Matt Galaviz <matthew.galaviz@gmail.com>
2019-02-18 21:59:59 -0800

    Initial: First commit. Code is working as intended.

---
+++ js/factory.js
@@ -0,0 +1,46 @@
+function DataFlowDiagram(svg_context) {
+    this.svg = svg_context;
+    this.children = [];
+}
+
+DataFlowDiagram.prototype.appendSingle = function(child) {
+    this.children.push(child);
+}
+
+DataFlowDiagram.prototype.appendMulti = function(children) {
+    for (var i = 0; i < children.length; i++) {
+        var child = children[i];
+        this.appendSingle(child);
+    }
+}
+
+DataFlowDiagram.prototype.render = function() {
+    for(i = 0; i < this.children.length; i++) {
+        child = this.children[i];
+        this.svg.appendChild(child);
+    }
+}
+
+function create_db(x, y, width, height) {
+    const items = [];
+    items.push(create_data_top(x, y, width, height));
+    items.push(create_data_band(x, y, width, height));
+    return items;
+}
+
+function create_object_store(x, y, width, height) {
+    const items = [];
+    items.push(create_data_top(x, y, width, height));
+    items.push(create_flexible_band(x, y, width, height, 25));
+    return items;
+}
+
+function create_file_store(x, y, width, height) {
+    const items = [];
+    items.push(create_data_top(x, y, width, height));
+    const tri_height = height / 3;
+    items.push(create_data_band(x, y, width, tri_height));
+    items.push(create_data_band(x, y + tri_height + 5, width, tri_height));
+    items.push(create_data_band(x, (y + 2 * tri_height) + 10, width, tri_height));
+    return items;
+}
---
+++ js/utils.js
@@ -0,0 +1,84 @@
+function create_data_top(x, y, width, height) {
+    const w = width.toString();
+    const hw = (width / 2).toString();
+    const offset = -5;
+    const y_adjusted = parseInt(y) + offset;
+    var element = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    element.setAttribute('d', 'm' + x + ',' + y_adjusted.toString() + ' a' + hw +
+        ',20 0 1,0 ' + w + ',0a' + hw + ',20 0 0,0 -' + w + ',0');
+    return element;
+}
+
+function create_flexible_band(x, y, width, height, slim) {
+    const h = height.toString();
+    const w = width.toString();
+    const iwidth = parseInt(width);
+
+    const nw = iwidth - (slim * 2);    
+    const hnw = (nw / 2).toString();
+
+    const hw = (iwidth / 2).toString();
+    const narrow_slim = '-' + slim.toString();
+    const narrow_width = '-' + (iwidth - (slim * 2)).toString();
+    var element = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path_str = 'm' + x + ',' + y + ' a' + hw + ',20 0 1,0 ' + w +
+    // ',0 l' + narrow_slim + ',' + h + ' a' + hnw + ',0 0 1,1 ' + narrow_width + ',0'
+    ',0 l' + narrow_slim + ',' + h + ' a' + hnw + ',13 0 1,1 ' + narrow_width + ',0'
+    element.setAttribute('d', path_str);
+    return element;
+}
+
+function create_data_band(x, y, width, height) {
+    const h = height.toString();
+    const w = width.toString();
+    const hw = (parseInt(width) / 2).toString();
+    var element = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path_str = 'm' + x + ',' + y + ' a' + hw + ',20 0 1,0 ' + w +
+        ',0 l0,' + h + ' a' + hw + ',20 0 1,1 -' + w + ',0'
+    element.setAttribute('d', path_str);
+    return element;
+}
+
+function create_ellipse() {
+    var element = document.createElementNS('http://www.w3.org/2000/svg', 'ellipse');
+    element.setAttribute('cx', '100');
+    element.setAttribute('cy', '50');
+    element.setAttribute('rx', '100');
+    element.setAttribute('ry', '20');
+    return element;
+}
+
+function create_text() {
+    var element = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    element.setAttribute('x', '210');
+    element.setAttribute('y', '250');
+    element.setAttribute('font-size', '30');
+    element.setAttribute('font-family', 'Verdana');
+    element.setAttribute('fill', '#000000');
+    var txt = document.createTextNode("/123456789012345678901234567890");
+    element.appendChild(txt);
+    return element;
+}
+
+function create_rect() {
+    var element = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    element.setAttribute('x', '200');
+    element.setAttribute('y', '200');
+    element.setAttribute('width', '600');
+    element.setAttribute('height', '100');
+    element.setAttribute('stroke', 'black');
+    element.setAttribute('stroke-width', '3');
+    element.setAttribute('fill', 'rgb(0,0,255)');
+    return element;
+}
+
+function create_circle() {
+    var element = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    element.setAttribute('cx', '850');
+    element.setAttribute('cy', '250');
+    element.setAttribute('r', '40');
+    element.setAttribute('stroke', 'green');
+    element.setAttribute('stroke-width', '4');
+    element.setAttribute('fill', 'red');
+    return element;
+}
